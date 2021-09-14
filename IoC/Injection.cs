1b64fb107df2e1aceb05d53286ce2ee2093ad95b
Joao Luiz <jmuciaccia@tecnologia-unica.com.br>
2021-09-14 10:49:33 -0300

    Apontando para Azure

--- IoC/Injection.cs
+++ IoC/Injection.cs
@@ -16,7 +16,7 @@ namespace IoC
         {
             // Repository
             services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddTransient<IDbConnection>(_ => new SqlConnection("Data Source=JOAOLUIZ;Initial Catalog=identityDB;Integrated Security=True"));
+            services.AddTransient<IDbConnection>(_ => new SqlConnection("Server=tcp:noticia-bd.database.windows.net,1433;Initial Catalog=noticia-base;Persist Security Info=False;User ID=user157923admin;Password=nOv5vMJ&n!@cH2l;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
             services.AddTransient<IPessoaFisicaRepository, PessoaFisicaRepository>();
             services.AddTransient<IPessoaJuridicaRepository, PessoaJuridicaRepository>();
             services.AddTransient<IUsuarioRepository, UsuarioRepository>();
