7c43ef09ba58cc385b71d4c68e2b11f590897574
Joao Luiz <jmuciaccia@tecnologia-unica.com.br>
2021-09-16 11:47:43 -0300

    BASE APPSERVE

---
+++ Application/AppServices/BaseAppService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Interfaces.Repository;
+
+namespace Application.AppServices
+{
+    public class BaseAppService
+    {
+        protected readonly IUnitOfWork _unitOfWork;
+
+        public BaseAppService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
