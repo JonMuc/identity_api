6601d9f0bd72e0928c98e3825c3c9c224a96593b
João Victor Abreu Cunha <joao.v.cunha95@gmail.com>
2021-09-16 10:12:24 -0300

    teste commit

---
+++ Application/AppServices/UsuarioAppService.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Models;
+using Domain.Services;
+
+namespace Application.AppServices
+{
+    public class UsuarioAppService
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioAppService(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        public ResponseViewModel AdicionarPessoaFisica(PessoaFisica pessoa)
+        {
+            return _usuarioService.AdicionarPessoaFisica(pessoa);
+        }
+
+        public ResponseViewModel ListarPessoaFisica()
+        {
+            return _usuarioService.ListarPessoaFisica();
+        }
+
+        public ResponseViewModel AtualizarPessoaFisica(PessoaFisica pessoa)
+        {
+            return _usuarioService.AtualizarPessoaFisica(pessoa);
+        }
+
+        public ResponseViewModel DeletarPessoaFisica(PessoaFisica pessoa)
+        {
+            return _usuarioService.DeletarPessoaFisica(pessoa);
+        }
+
+        public ResponseViewModel BuscarPessoaFisica(long idPessoa)
+        {
+            return _usuarioService.BuscarPessoaFisica(idPessoa);
+        }
+    }
+}
---
+++ Domain/Interfaces/Services/IUsuarioService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace Domain.Services
+{
+    public interface IUsuarioService
+    {
+        ResponseViewModel AdicionarPessoaFisica(PessoaFisica pessoa);
+        ResponseViewModel ListarPessoaFisica();
+        ResponseViewModel AtualizarPessoaFisica(PessoaFisica pessoa);
+        ResponseViewModel DeletarPessoaFisica(PessoaFisica pessoa);
+        ResponseViewModel BuscarPessoaFisica(long idPessoa);
+    }
+}
