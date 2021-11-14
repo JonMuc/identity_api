3f38f49bc0944ebb0061b378348328e4591a2f76
Joao Luiz <jmuciaccia@tecnologia-unica.com.br>
2021-11-14 13:11:40 -0300

    ajuste exibir comentarios

--- Data/Repository/ComentarioRepository.cs
+++ Data/Repository/ComentarioRepository.cs
@@ -23,7 +23,10 @@ namespace Data.Repository
 
         public async Task<IEnumerable<ViewComentario>> ListarComentariosNoticiaAsync(long idNoticia)
         {
-            var sql = @"select come.Id as IdComentario, usua.Id as IdUsuario, come.Mensagem, usua.Nome, usua.Foto, come.CriadoEm as DataComentario
+            var sql = @"select come.Id as IdComentario, usua.Id as IdUsuario, come.Mensagem, usua.Nome, usua.Foto as UrlFoto, come.CriadoEm as DataComentario,
+                        (select count(*) from TBL_AVALIACAO where IdComentario = come.Id and TipoAvaliacao = 1 and StatusRegistro = 0) as QuantidadeLike,
+                        (select count(*) from TBL_AVALIACAO where IdComentario = come.Id and TipoAvaliacao = 2 and StatusRegistro = 0) as QuantidadeDeslike,
+                        come.IdComentario as ComentarioFilho
                         from TBL_COMENTARIO come
                         inner join TBL_USUARIO usua
                         on come.IdCriadoPor = usua.Id WHERE come.IdNoticia = @idNoticia";
--- Domain/Models/Dto/ViewComentario.cs
+++ Domain/Models/Dto/ViewComentario.cs
@@ -8,7 +8,10 @@ namespace Domain.Models.Dto
         public long IdUsuario { get; set; }
         public string Mensagem { get; set; }
         public string Nome { get; set; }
-        public string Foto { get; set; }
+        public string UrlFoto { get; set; }
         public DateTime DataComentario { get; set; }
+        public long QuantidadeLike { get; set; }
+        public long QuantidadeDeslike { get; set; }
+        public bool ComentarioFilho { get; set; }
     }
 }
