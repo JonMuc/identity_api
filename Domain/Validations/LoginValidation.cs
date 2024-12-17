6a35c370c658b96fb25eb4fd7a7817492ba73241
JonMuc <joaomuciaccia@gmail.com>
2024-12-17 09:25:03 -0300

    commit parcial

--- Domain/Validations/LoginValidation.cs
+++ Domain/Validations/LoginValidation.cs
@@ -1,37 +1,37 @@
-﻿using Domain.Interfaces;
-using Domain.Models;
-using Domain.Models.Request;
-using Domain.Util;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Util.Criptografia;
+﻿//using Domain.Interfaces;
+//using Domain.Models;
+//using Domain.Models.Request;
+//using Domain.Util;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using Util.Criptografia;
 
-namespace Domain.Validations
-{
-    public class LoginValidation
-    {
-        private readonly IUsuarioRepository _usuarioRepository;
+//namespace Domain.Validations
+//{
+//    public class LoginValidation
+//    {
+//        private readonly IUsuarioRepository _usuarioRepository;
 
-        public LoginValidation(IUsuarioRepository usuarioRepository)
-        {
-            _usuarioRepository = usuarioRepository;
-        }
+//        public LoginValidation(IUsuarioRepository usuarioRepository)
+//        {
+//            _usuarioRepository = usuarioRepository;
+//        }
 
-        public async Task<Usuario> LoginUsuarioAsync(LoginRequest request)
-        {
-            var errosResponse = new List<string>(0);
-            var usuario = await _usuarioRepository.GetUsuarioByEmailAsync(request.Email);
-            if (usuario == null)
-            {
-                errosResponse.Add("Email não encontrado.");
-                throw new ParametroException(errosResponse);
-            }
-            if (CryptographyHelper.DecryptString(usuario.Senha) != request.Senha)
-            {
-                errosResponse.Add("Email e senha não conferem.");
-                throw new ParametroException(errosResponse);
-            }
-            return usuario;
-        }
-    }
-}
+//        public async Task<Usuario> LoginUsuarioAsync(LoginRequest request)
+//        {
+//            var errosResponse = new List<string>(0);
+//            var usuario = await _usuarioRepository.GetUsuarioByEmailAsync(request.Email);
+//            if (usuario == null)
+//            {
+//                errosResponse.Add("Email não encontrado.");
+//                throw new ParametroException(errosResponse);
+//            }
+//            if (CryptographyHelper.DecryptString(usuario.Senha) != request.Senha)
+//            {
+//                errosResponse.Add("Email e senha não conferem.");
+//                throw new ParametroException(errosResponse);
+//            }
+//            return usuario;
+//        }
+//    }
+//}
